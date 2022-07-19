39322b11d43fbeda008a6ecc95c3f82fe0f00b0f
Bertrand Warot <bertrand.warot@ejara.africa>
2022-07-19 23:50:38 +0200

    Fun with embeds and sticky messages

---
+++ config/config.py
@@ -0,0 +1,4 @@
+import os
+
+working_environment = os.environ['SOJ_WORKING_ENVIRONMENT']
+discord_bot_token = os.environ['SOJ_DISCORD_BOT_TOKEN']
--- handler.py
+++ handler.py
@@ -1,26 +1,62 @@
-import discord
+from config import config
+import disnake
+from disnake.ext import commands
 
-TOKEN = ''
+bot = commands.Bot(
+    command_prefix='!',
+    test_guilds=[935132340538212413]
+)
 
-client = discord.Client()
+TOKEN = config.discord_bot_token
 
-@client.event
+
+
+
+@bot.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print('We have logged in as {0.user}'.format(bot))
 
-@client.event
+@bot.event
 async def on_message(message):
-    if message.author == client.user:
+    if message.author == bot.user:
         return
+    message_embed = disnake.Embed(
+        title=f'Bienvenue sur {message.channel.name} !',
+        description="Ceci est un sticky message pour vous rappeler des choses de base, tout ça tout ça.",
+        colour=0xF0C43F,
+    )
+    print(message_embed)
+    async for msg in message.channel.history(limit=20):
+        if message_embed in msg.embeds and msg.author == bot.user:
+            await msg.delete()
+    await message.channel.send(embed=message_embed)
 
-    if message.content.startswith('Hello bot!'):
-        await message.channel.send('Hello World!')
 
-@client.event
+@bot.event
 async def on_reaction_add(reaction, user):
-    if user == client.user:
+    if user == bot.user:
         return
 
     await reaction.message.channel.send(reaction.emoji)
 
-client.run(TOKEN)
+@bot.slash_command(description="respons to an idiot")
+async def coucou(inter):
+    await inter.response.send_message("salut !")
+    async for message in inter.channel.history(limit=100):
+        print(message.content)
+
+@bot.slash_command(description="Testing embeds")
+async def embed(inter):
+    embed = disnake.Embed(
+        title="Embed Title",
+        description="Embed Description",
+        colour=0xF0C43F,
+    )
+    await inter.response.send_message('coucou', embed=embed)
+
+
+bot.run(TOKEN)
+
+
+
+# https://discord.com/api/oauth2/authorize?client_id=999045651893600386&permissions=0&scope=bot%20applications.commands
